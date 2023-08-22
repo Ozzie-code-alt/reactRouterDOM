81365a7c363912ce5104d7a5580f5d9773a22e75
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-22 22:35:23 +0800

    changes

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,5 +1,5 @@
 
-import {Route,Routes } from 'react-router-dom'
+import {Link,Route,Routes } from 'react-router-dom'
 import About from './pages/About'
 import Book from './pages/Book'
 function App() {
@@ -7,6 +7,12 @@ function App() {
 
   return (
     <>
+    <nav>
+      <ul>
+      <li><Link to='/'>About</Link></li>
+      <li><Link to='/book'>Books</Link></li> {/*Link acts as new acnhor Tag */}
+      </ul>
+    </nav>
       <Routes> 
       <Route/>
       <Route path='/' element={<About/>}/> 
